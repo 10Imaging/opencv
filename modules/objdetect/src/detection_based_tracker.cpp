9e4195c4246bcc822cbc43d7b65027638a64c6e3
Victor Vedovato <victor@10imaging.com>
2018-09-12 16:01:16 -0700

    Revert "shari's blender and tracker patches"
    This reverts commit 163303a3523978a92f84d75a5334b34190381071.

--- modules/objdetect/src/detection_based_tracker.cpp
+++ modules/objdetect/src/detection_based_tracker.cpp
@@ -403,7 +403,6 @@ void cv::DetectionBasedTracker::SeparateDetectionWork::workcycleObjectDetector()
 
         int64 t1_detect=getTickCount();
 
-        LOGD("Calling detect");
         cascadeInThread->detect(imageSeparateDetecting, objects);
 
         /*cascadeInThread.detectMultiScale( imageSeparateDetecting, objects,
@@ -589,7 +588,7 @@ cv::DetectionBasedTracker::Parameters::Parameters()
 cv::DetectionBasedTracker::InnerParameters::InnerParameters()
 {
     numLastPositionsToTrack=4;
-    numStepsToWaitBeforeFirstShow=0;
+    numStepsToWaitBeforeFirstShow=6;
     numStepsToTrackWithoutDetectingIfObjectHasNotBeenShown=3;
     numStepsToShowWithoutDetecting=3;
 
@@ -627,7 +626,6 @@ cv::DetectionBasedTracker::~DetectionBasedTracker()
 
 void DetectionBasedTracker::process(const Mat& imageGray)
 {
-    LOGD("DetectionBasedTracker::process() -- start");
     CV_INSTRUMENT_REGION()
 
     CV_Assert(imageGray.type()==CV_8UC1);
